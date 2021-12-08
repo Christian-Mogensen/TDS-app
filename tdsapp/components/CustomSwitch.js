6e4e39974c45c454c22ffc021d7b2691b33283af
Christian-Mogensen <U2407192@rts-365.dk>
2021-12-08 12:34:59 +0100

    Add light dark theme logic

--- tdsapp/components/CustomSwitch.js
+++ tdsapp/components/CustomSwitch.js
@@ -1,6 +1,12 @@
 import clsx from 'clsx';
 import { styled } from '@mui/system';
 import { useSwitch } from '@mui/base/SwitchUnstyled';
+import useDarkMode from "../hooks/useDarkMode";
+import { useState, useEffect } from 'react';
+
+
+
+
 
 const SwitchRoot = styled('span')`
   display: inline-block;
@@ -26,7 +32,7 @@ const SwitchThumb = styled('span')(
   ({ theme }) => `
   position: absolute;
   display: block;
-  background-color: ${theme.palette.mode === 'dark' ? '#003892' : '#001e3c'};
+  background-color: ${theme === 'light' ? '#e5e7eb' : '#111827'};
   width: 32px;
   height: 32px;
   border-radius: 16px;
@@ -40,7 +46,7 @@ const SwitchThumb = styled('span')(
     width: 100%;
     height: 100%;
     background: url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" height="20" width="20" viewBox="0 0 20 20"><path fill="${encodeURIComponent(
-      '#fff',
+      theme === 'light' ? '#374151' : '#e5e7eb',
     )}" d="M9.305 1.667V3.75h1.389V1.667h-1.39zm-4.707 1.95l-.982.982L5.09 6.072l.982-.982-1.473-1.473zm10.802 0L13.927 5.09l.982.982 1.473-1.473-.982-.982zM10 5.139a4.872 4.872 0 00-4.862 4.86A4.872 4.872 0 0010 14.862 4.872 4.872 0 0014.86 10 4.872 4.872 0 0010 5.139zm0 1.389A3.462 3.462 0 0113.471 10a3.462 3.462 0 01-3.473 3.472A3.462 3.462 0 016.527 10 3.462 3.462 0 0110 6.528zM1.665 9.305v1.39h2.083v-1.39H1.666zm14.583 0v1.39h2.084v-1.39h-2.084zM5.09 13.928L3.616 15.4l.982.982 1.473-1.473-.982-.982zm9.82 0l-.982.982 1.473 1.473.982-.982-1.473-1.473zM9.305 16.25v2.083h1.389V16.25h-1.39z"/></svg>') center center no-repeat;
   }
 
@@ -53,38 +59,61 @@ const SwitchThumb = styled('span')(
     
     &:before {
       background-image: url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" height="20" width="20" viewBox="0 0 20 20"><path fill="${encodeURIComponent(
-        '#fff',
+        theme === 'light' ? '#374151' : '#e5e7eb',
       )}" d="M4.2 2.5l-.7 1.8-1.8.7 1.8.7.7 1.8.6-1.8L6.7 5l-1.9-.7-.6-1.8zm15 8.3a6.7 6.7 0 11-6.6-6.6 5.8 5.8 0 006.6 6.6z"/></svg>');
     }
   }
 `,
 );
 
+
 const SwitchTrack = styled('span')(
   ({ theme }) => `
-  background-color: ${theme.palette.mode === 'dark' ? '#8796A5' : '#aab4be'};
+  background-color: ${!theme === 'light' ? '#171f35' : 'gray'};
   border-radius: 10px;
   width: 100%;
   height: 100%;
   display: block;
+  box-shadow: inset 3px 2px 4px 1px #00000075
 `,
 );
 
-function MUISwitch(props) {
+function MUISwitch(props, {theme}) {
   const { getInputProps, checked, disabled, focusVisible } = useSwitch(props);
 
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      const root = window.document.documentElement;
+      let theme;
+      if (localStorage) {
+        theme = localStorage.getItem("theme");
+        root.classList.add(localStorage.theme);
+      }
+    }
+  }, [theme]);
+  
   const stateClasses = {
     checked,
     disabled,
     focusVisible,
   };
+  const [colorTheme, setTheme] = useDarkMode();
+  const [themeTxt, setThemeTxt] = useState(false);
+  function themeToggle () {
+    setTheme(colorTheme)
+    setThemeTxt(themeTxt)
+    if (typeof window !== "undefined") {
+      localStorage.setItem('theme', colorTheme)
+     }
+     console.log(localStorage.theme);
+  }
 
   return (
-    <SwitchRoot className={clsx(stateClasses)}>
+    <SwitchRoot className={clsx(stateClasses)} onClick={themeToggle}>
       <SwitchTrack>
-        <SwitchThumb className={clsx(stateClasses)} />
+        <SwitchThumb className={clsx(stateClasses)} theme={colorTheme} />
       </SwitchTrack>
-      <SwitchInput {...getInputProps()} aria-label="Demo switch" />
+      <SwitchInput {...getInputProps()} theme={colorTheme}  />
     </SwitchRoot>
   );
 }
--- tdsapp/pages/_app.js
+++ tdsapp/pages/_app.js
@@ -2,6 +2,14 @@ import '../styles/styles.css'
 
 
 function MyApp({ Component, pageProps }) {
+  if (typeof window !== "undefined") {
+    const root = window.document.documentElement
+    let theme;
+    if (localStorage) {
+    theme = localStorage.getItem("theme")
+    root.classList.add(localStorage.theme);
+    }
+    }
   return <Component {...pageProps} />
 }
 
