5a37d30ac3218ac2c928e89e2fedee19f21375fb
Christian-Mogensen <u2407192@rts-365.dk>
2022-05-17 21:03:58 +0200

    rename file

--- tdsapp/pages/index.js
+++ tdsapp/pages/index.js
@@ -1,15 +1,6 @@
-import Wrapper from "../components/Wrapper";
+import { AnimatePresence } from "framer-motion";
+import Calendar from "../components/Calendar/Calendar";
 import Header from "../components/Header";
-import Main from "../components/Main";
-import Image from "next/image";
-import Footer from "../components/Footer";
-import Link from "next/link";
-import { useEffect } from "react";
-import { getDate, format, getDayOfYear, isLeapYear } from "date-fns";
-import { motion, AnimatePresence } from "framer-motion";
-import LoginBar from "../components/LoginBar";
-import { ThemeProvider } from "../context/ThemeToggle";
-import Calendar from "../components/Calendar";
 import { useStateContext } from "../context/stateContext";
 import LandingPage from "./landingpage";
 
